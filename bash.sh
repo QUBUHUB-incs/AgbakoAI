7a4e0a63064d8d3f9342008f227d80e825035c98
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-02 03:15:32 +0100

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash.sh
+++ bash.sh
@@ -47,6 +47,23 @@ pip install -r requirements.txt
 # For Node.js dependencies
 npm install
 
+
+set -e
+rm -rf ./vendor/cmd/go
+cp -a $(go env GOROOT)/src/cmd/go vendor/cmd/go
+rm -f vendor/cmd/go/alldocs.go vendor/cmd/go/mkalldocs.sh # docs are in wrong place and describe wrong command
+cd vendor/cmd/go
+patch -p0 < ../../../patch.txt
+vers=$(go version | sed 's/^go version //; s/ [A-Z][a-z][a-z].*//')
+echo "package version; const version = \"$vers\"" > internal/version/vgo.go
+gofmt -w internal
+cd ../../..
+rm $(find . -name '*.orig')
+go build
+./vgo version
+rm vgo
+git add .
+
 git status
 git add agbakoAI.py .gitignore requirements.txt  # add more files if needed
 git commit -m "Initial commit of AgbakoAI project files"
