d5e44b6a80c2ce6e6baf5d6ecc4bade7a8f919ca
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-02 01:17:27 +0100

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash.sh
+++ bash.sh
@@ -1,1 +1,5 @@
 pip install speechrecognition transformers pyttsx3 torch
+pip install fastapi uvicorn torch
+pip install fastapi uvicorn torch
+uvicorn neo_server_real:app --reload --host 0.0.0.0 --port 8000
+uvicorn neo_server_real:app --reload --host 0.0.0.0 --port 8000
